858b40bcc6759189fce018c565fabd51a8fa32f9
Aakash Verma <aakashvermaee@gmail.com>
2020-03-31 09:25:05 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'amazon-content',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
